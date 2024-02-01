46a894f0a59e3e8342c1fadccdd0627e7ed4f65c
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-01 17:16:34 +0200

    pyramid rule handler - not working yet

---
+++ Taki/Game/GameRules/PyramidRuleHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taki.Game.Deck;
+using Taki.Game.Players;
+
+namespace Taki.Game.GameRules
+{
+    internal class PyramidRuleHandler(PlayerHandler playerHandler, CardDeck cardDeck) : RuleHandler(playerHandler, cardDeck)
+    {
+        protected override bool PlayerFinishedHand()
+        {
+            if (base.PlayerFinishedHand())
+            {
+                PyramidPlayer player = (PyramidPlayer)playerHandler.CurrentPlayer;
+                if (player.CurrentNumberOfCards() == 0)
+                    return true;
+                playerHandler.DrawCards(player.CurrentNumberOfCards() - 1, cardDeck);
+                player.CurrentCardsMinus1();
+                throw new Exception("check if this is working");
+                return false;
+            }
+            return false;
+        }
+    }
+}
