4dc2d12f85de5fd8423daa9d65b937b59d78a42b
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-01 12:22:58 +0200

    some notes about what to do with card deck handler

--- Taki/Game/GameRules/CardDeckHandler.cs
+++ Taki/Game/GameRules/CardDeckHandler.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Taki.Game.Deck;
 
 namespace Taki.Game.GameRules
 {
-    internal class CardDeckHandler
+    internal class CardDeckHandler(CardDeck cardDeck)
     {
+        //we can save all of the important things like color change,
+        //previous unique card
+        //more
+        private readonly CardDeck cardDeck = cardDeck;
     }
 }
