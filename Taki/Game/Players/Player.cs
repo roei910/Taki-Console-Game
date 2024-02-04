831d738bb64090ddadd694e9ecff7b48c6f2e5f8
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-04 17:02:54 +0200

    message to keep track of color change

--- Taki/Game/Players/Player.cs
+++ Taki/Game/Players/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
@@ -44,7 +45,9 @@ namespace Taki.Game.Players
 
         public Color ChooseColor()
         {
-            return choosingAlgorithm.ChooseColor(this);
+            Color color = choosingAlgorithm.ChooseColor(this);
+            Debug.WriteLine($"Player[{Id}] chose color: {color}");
+            return color;
         }
 
         public void AddCard(Card card)
