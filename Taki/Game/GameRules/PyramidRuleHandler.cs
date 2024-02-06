7c3bfd3186a7c7595915646136a1c09bb5dd6750
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-06 11:11:39 +0200

    fixing error where the pyramid player want to draw 0 cards and then throws exception

--- Taki/Game/GameRules/PyramidRuleHandler.cs
+++ Taki/Game/GameRules/PyramidRuleHandler.cs
@@ -18,12 +18,16 @@ namespace Taki.Game.GameRules
                 PyramidPlayer player = (PyramidPlayer)playerHandler.CurrentPlayer;
                 if (player.CurrentNumberOfCards() == 0)
                     return true;
-                if (!playerHandler.DrawCards(player.CurrentNumberOfCards() - 1, cardDeck))
-                    throw new Exception("player cannot get more cards and is stuck!");
+                
                 player.CurrentCardsMinus1();
                 if(player.CurrentNumberOfCards() != 0)
+                {
+                    if (!playerHandler.DrawCards(player.CurrentNumberOfCards(), cardDeck))
+                        throw new Exception("player cannot get more cards and is stuck!");
                     Utilities.PrintConsoleError($"Player[{player.Id}] finished his current hand," +
                         $" currently on {player.CurrentNumberOfCards()} card(s)");
+                }
+                    
                 if(CurrentTakiCard != null)
                 {
                     CurrentTakiCard = null;
