6528e36c505208ec9d4cbc300d8df077133053c5
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-06 16:26:23 +0200

    created copy constructor for CardDeck

--- Taki/Game/Deck/CardDeck.cs
+++ Taki/Game/Deck/CardDeck.cs
@@ -20,6 +20,13 @@ namespace Taki.Game.Deck
             ShuffleDeck();
         }
 
+        public CardDeck(CardDeck cardDeck)
+        {
+            discardPile = new LinkedList<Card>(discardPile);
+            drawPile = new LinkedList<Card>(discardPile);
+            ShuffleDeck();
+        }
+
         public Card GetTopDiscardPile()
         {
             try
