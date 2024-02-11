fb9045ab42187e5a31ab1b7f6c246a90785d6dea
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-11 15:44:22 +0200

    moving Imessage handler to interfaces

--- Taki/Game/Interfaces/IMessageHandler.cs
+++ Taki/Game/Interfaces/IMessageHandler.cs
@@ -1,10 +1,10 @@
-﻿namespace Taki.Game.Communicators
+﻿namespace Taki.Game.Interfaces
 {
     internal interface IMessageHandler
     {
         void SendErrorMessage(string message);
         void SendAlertMessage(string message);
-        void SendMessageToUser(string message);
+        void SendMessageToUser(object? message);
         int GetNumberFromUser();
         string? GetMessageFromUser();
         void SendMessageToUser();
