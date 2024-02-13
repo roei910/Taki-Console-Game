1486df2846f74f03575811b1cf87577114e7aa47
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-13 09:31:34 +0200

    cleaning game runner factory, now using other factories

--- Taki/Game/Factories/TakiGameFactory.cs
+++ Taki/Game/Factories/TakiGameFactory.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Taki.Game.General;
+using Taki.Game.GameRules;
 using Taki.Game.Handlers;
 using Taki.Game.Interfaces;
 using Taki.Game.Managers;
@@ -20,62 +20,39 @@ namespace Taki.Game.Factories
 
         private readonly IMessageHandler _messageHandler;
         private readonly IServiceProvider _serviceProvider;
-        private readonly Utilities _utilities;
 
         public TakiGameFactory(IServiceProvider serviceProvider)
         {
             _messageHandler = serviceProvider.GetRequiredService<IMessageHandler>();
             _serviceProvider = serviceProvider;
-            _utilities = serviceProvider.GetRequiredService<Utilities>();
         }
 
-        public TakiGameRunner ChooseTypeOfGame(GameHandlers gameHandlers)
+        internal TakiGameRunner ChooseTypeOfGame(PlayersHandlerFactory playersHandlerFactory, CardsHandlerFactory cardsHandlerFactory, ServiceProvider serviceProvider)
         {
-            GameTypeEnum typeOfGame = _utilities
-                .GetEnumFromUser<GameTypeEnum>("of game", GameTypeEnum.Normal.GetHashCode());
+            CardsHandler cardsHandler = cardsHandlerFactory.GenerateCardsHandler();
+
+            GameTypeEnum typeOfGame = _messageHandler
+                .GetEnumFromUser<GameTypeEnum>();
 
             switch (typeOfGame)
             {
                 case GameTypeEnum.Normal:
-                    int numberOfPlayers = gameHandlers.GetPlayersHandler().GetAllPlayers().Count;
-                    int numberOfPlayerCards = GetNumberOfPlayerCards(numberOfPlayers);
-                    return new TakiGameRunner(gameHandlers, _serviceProvider, numberOfPlayerCards);
-                
+                    PlayersHandler playerHandler = playersHandlerFactory.GeneratePlayersHandler();
+                    GameHandlers gameHandlers = new(playerHandler, cardsHandler, serviceProvider);
+
+                    return new TakiGameRunner(gameHandlers);
+
                 case GameTypeEnum.Pyramid:
-                    return new(gameHandlers, _serviceProvider);
-                
+                    PlayersHandler pyramidPlayersHandler = playersHandlerFactory.GeneratePyramidPlayersHandler();
+                    GameHandlers pyramidGameHandlers = new(pyramidPlayersHandler, cardsHandler, serviceProvider);
+                    
+                    return new TakiGameRunner(pyramidGameHandlers);
+
                 default:
                     throw new Exception("type enum was wrong");
             }
         }
 
-        private int GetNumberOfPlayerCards(int numberOfPlayers)
-        {
-            int maxNumberOfPlayerCards = CardsHandlerFactory.MaxNumberOfCards() / numberOfPlayers - 1;
-
-            if (maxNumberOfPlayerCards > MAX_NUMBER_OF_PLAYER_CARDS)
-                maxNumberOfPlayerCards = MAX_NUMBER_OF_PLAYER_CARDS;
-
-            _messageHandler.SendMessageToUser("please enter a number of player cards");
-            int numberOfPlayerCards = _messageHandler.GetNumberFromUser();
-
-            if (numberOfPlayerCards > maxNumberOfPlayerCards)
-            {
-                _messageHandler.SendMessageToUser($"Too many cards per player, max is {maxNumberOfPlayerCards}." +
-                    $" setting as the max value");
-
-                return maxNumberOfPlayerCards;
-            }
-
-            if (numberOfPlayerCards < MIN_NUMBER_OF_PLAYER_CARDS)
-            {
-                _messageHandler.SendMessageToUser($"Not enough cards per player, " +
-                    $"min is {MIN_NUMBER_OF_PLAYER_CARDS} setting as min value");
-
-                return MIN_NUMBER_OF_PLAYER_CARDS;
-            }
-
-            return numberOfPlayerCards;
-        }
+        
     }
 }
