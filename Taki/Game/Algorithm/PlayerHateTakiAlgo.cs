25e0020f5f94755677cacdb49809b86d8d798caa
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-13 11:17:54 +0200

    fixing player hate taki behavior when dealing with taki cards, going into infinite loop

--- Taki/Game/Algorithm/PlayerHateTakiAlgo.cs
+++ Taki/Game/Algorithm/PlayerHateTakiAlgo.cs
@@ -6,15 +6,23 @@ namespace Taki.Game.Algorithm
 {
     internal class PlayerHateTakiAlgo : PlayerAlgorithm
     {
+        bool IsTaki = false;
+
         public override Card? ChooseCard(Func<Card, bool> isSimilarTo,
             Player player, GameHandlers gameHandlers)
         {
             Card topDiscard = gameHandlers.GetCardsHandler().GetTopDiscard();
-
-            if (topDiscard is TakiCard)
+            if (IsTaki)
+            {
+                IsTaki = false;
                 return null;
+            }
 
-            return base.ChooseCard(isSimilarTo, player, gameHandlers);
+            Card? playerCard = base.ChooseCard(isSimilarTo, player, gameHandlers);
+            if (playerCard is TakiCard)
+                IsTaki = true;
+
+            return playerCard;
         }
 
         public override string ToString()
