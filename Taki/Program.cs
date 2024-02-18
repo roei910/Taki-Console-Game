5bd33281b2bd7f4f8e0b4448b298bc7519766f99
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-18 15:25:42 +0200

    adding tasks

--- Taki/Program.cs
+++ Taki/Program.cs
@@ -9,6 +9,7 @@ using Taki;
 using Taki.Game.Deck;
 using Taki.Game.GameRunner;
 
+// TODO: everything in dependecy injection
 var serviceProvider = new ServiceCollection()
     .AddSingleton<IUserCommunicator, ConsoleUserCommunicator>()
     .AddSingleton<IPlayerAlgorithm, PlayerAlgorithm>()
