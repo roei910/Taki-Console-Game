76d843621084eec8fd09045d843076ce0771e8a8
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-18 17:05:24 +0200

    adding tasks

--- Taki/Program.cs
+++ Taki/Program.cs
@@ -9,7 +9,15 @@ using Taki;
 using Taki.Game.Deck;
 using Taki.Game.GameRunner;
 
-// TODO: everything in dependecy injection
+//TODO: check exception ewhen restarting, after reset there was only 2 cards in deck: 1 null, cards are being deleted somehow
+//TODO: check the plus card is okay
+//TODO: check if shuffling works well
+//TODO: when drawing / not drawing sometimes the message is not correct => should be -1 to finish (in taki e.g)
+//TODO: check the id's of players => should not restart from high number
+//TODO: sometimes writes twice the message for restart => check why
+//TODO: add option to show all of the scores for every player in db
+//TODO: check taki stops well and goes to next player
+
 var serviceProvider = new ServiceCollection()
     .AddSingleton<IUserCommunicator, ConsoleUserCommunicator>()
     .AddSingleton<IPlayerAlgorithm, PlayerAlgorithm>()
