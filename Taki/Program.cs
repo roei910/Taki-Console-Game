9b4f246048d6393cc657ef934244f83e0888053d
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-19 09:46:42 +0200

    finishing tasks. super taki should not implement taki due to stacking issues

--- Taki/Program.cs
+++ Taki/Program.cs
@@ -9,9 +9,8 @@ using Taki;
 using Taki.Game.GameRunner;
 
 //TODO: check exception ewhen restarting, after reset there was only 2 cards in deck: 1 null, cards are being deleted somehow
-//TODO: when drawing / not drawing sometimes the message is not correct => should be -1 to finish (in taki e.g)
 //TODO: check the id's of players => should not restart from high number
-//TODO: change super taki to implement taki
+//TODO: add error message after using change cards
 
 var serviceProvider = new ServiceCollection()
     .AddSingleton<IUserCommunicator, ConsoleUserCommunicator>()
