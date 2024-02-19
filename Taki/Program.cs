1c93ccd39d50ccf67701cc56c75ad4fb5ef95eb7
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-19 10:44:08 +0200

    removing finished tasks

--- Taki/Program.cs
+++ Taki/Program.cs
@@ -8,10 +8,6 @@ using Microsoft.Extensions.Configuration;
 using Taki;
 using Taki.Game.GameRunner;
 
-//TODO: check exception ewhen restarting, after reset there was only 2 cards in deck: 1 null, cards are being deleted somehow
-//TODO: check the id's of players => should not restart from high number
-//TODO: add error message after using change cards
-
 var serviceProvider = new ServiceCollection()
     .AddSingleton<IUserCommunicator, ConsoleUserCommunicator>()
     .AddSingleton<IPlayerAlgorithm, PlayerAlgorithm>()
