3818a9a64cb68c9536c61fd0d60b35e5742c3d89
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-19 09:06:15 +0200

    removing tasks after checking

--- Taki/Program.cs
+++ Taki/Program.cs
@@ -9,12 +9,9 @@ using Taki;
 using Taki.Game.GameRunner;
 
 //TODO: check exception ewhen restarting, after reset there was only 2 cards in deck: 1 null, cards are being deleted somehow
-//TODO: check the plus card is okay
-//TODO: check if shuffling works well
 //TODO: when drawing / not drawing sometimes the message is not correct => should be -1 to finish (in taki e.g)
 //TODO: check the id's of players => should not restart from high number
-//TODO: add option to show all of the scores for every player in db
-//TODO: check taki stops well and goes to next player
+//TODO: change super taki to implement taki
 
 var serviceProvider = new ServiceCollection()
     .AddSingleton<IUserCommunicator, ConsoleUserCommunicator>()
