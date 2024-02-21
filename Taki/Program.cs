a1e49125df90f360c7c95fca0abf4b1d76d86114
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-21 14:12:49 +0200

    adding databases in DI

--- Taki/Program.cs
+++ Taki/Program.cs
@@ -9,8 +9,14 @@ using Taki;
 using Taki.Game.GameRunner;
 using Taki.Game.Database;
 using Taki.Game.Players;
+using Taki.Game.Cards;
 
+//TODO: add check to see names are unique
 //TODO: make it possible to print many cards in a row of same color for manual choose, use the get stringArray method and add the strings
+//TODO: create mongo db collection to save every detail about the game => restart from the same place you stopped before!
+//TODO: continue saving the players cards and card decks in database
+//TODO: issue with restarting the top card doesnt know if there is anything before him, not being saved. save the previous if needed
+//TODO: not saving which player is currently playing
 
 var serviceProvider = new ServiceCollection()
     .AddSingleton<IUserCommunicator, ConsoleUserCommunicator>()
@@ -27,7 +33,19 @@ var serviceProvider = new ServiceCollection()
         .AddJsonFile("AppConfigurations.json", false, true)
         .Build())
     .AddSingleton<TakiGameRunner>()
-    .AddSingleton<IDatabase<Player>, PlayerDatabase>()
+    .AddSingleton<IDatabase<PlayerDTO>, PlayerDatabase>()
+    .AddKeyedSingleton<IDatabase<CardDTO>, CardDatabase>("drawPile", (serviceProvider, x) =>
+        {
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            return new CardDatabase(configuration, "DrawPile");
+        })
+    .AddKeyedSingleton<IDatabase<CardDTO>, CardDatabase>(
+        "discardPile", (serviceProvider, x) =>
+        {
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            return new CardDatabase(configuration, "DiscardPile");
+        })
+    .AddSingleton<TakiGameDatabaseHolder>()
     .BuildServiceProvider();
 
 TakiGameRunner gameRunner = serviceProvider.GetRequiredService<TakiGameRunner>();
