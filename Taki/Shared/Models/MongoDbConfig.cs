43103ba939f29b58c01db30406902d0037e87f75
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-26 18:18:37 +0200

    creating mongoDbConfig file for later use

---
+++ Taki/Shared/Models/MongoDbConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Taki.Shared.Models
+{
+    internal class MongoDbConfig
+    {
+        public string MongoUrl { get; set; } = null!;
+        public string MongoDatabaseName { get; set; } = null!;
+        public string DrawPileCollectionName { get; set; } = null!;
+        public string DiscardPileCollectionName { get; set; } = null!;
+        public string PlayersCollectionName { get; set; } = null!;
+        public string GameSettingsCollectionName { get; set; } = null!;
+    }
+}
