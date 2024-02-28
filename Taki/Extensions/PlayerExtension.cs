6f84c4c644a1e85da6920e4740e0aed6580ac210
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-28 16:51:17 +0200

    creating player model and creating extensions for card and player

---
+++ Taki/Extensions/PlayerExtension.cs
@@ -0,0 +1,15 @@
+﻿using Taki.Shared.Models.Dto;
+using Taki.Shared.Models;
+
+namespace Taki.Extensions
+{
+    public static class PlayerExtension
+    {
+        public static PlayerDto ToPlayerDto(this Player player)
+        {
+            List<CardDto> cards = player.PlayerCards.Select(card => card.ToCardDto()).ToList();
+
+            return new PlayerDto(player.Score, player.Name!, player.Id, cards, player.ChoosingAlgorithm!);
+        }
+    }
+}
---
+++ Taki/Shared/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using Taki.Shared.Abstract;
+
+namespace Taki.Shared.Models
+{
+    public class Player
+    {
+        public ObjectId Id { get; set; } = new ObjectId();
+        public int Score { get; set; }
+        public string? Name { get; set; }
+        public string? ChoosingAlgorithm { get; set; }
+        public List<Card> PlayerCards { get; set; } = [];
+        public int NumberOfPlayerCards { get; set; }
+    }
+}
