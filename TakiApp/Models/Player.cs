525497fe0ce106240b0583b5eb8240d68e938c46
roei910 <74923231+roei910@users.noreply.github.com>
2024-03-04 10:07:48 +0200

    adding score and cards to player model

--- TakiApp/Models/Player.cs
+++ TakiApp/Models/Player.cs
@@ -5,10 +5,11 @@ namespace TakiApp.Models
     public class Player
     {
         public ObjectId Id { get; set; }
-        public List<Card> Cards { get; set; } = [];
         public string? Name { get; set; }
         public string? PlayerAlgorithm { get; set; }
         public DateTime? LastCheckIn { get; set; }
         public bool IsPlaying { get; set; } = false;
+        public int Score { get; set; } = 0;
+        public List<Card> Cards { get; set; } = [];
     }
 }
