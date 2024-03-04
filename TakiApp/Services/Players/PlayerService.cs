ae823e14dd6648a49d55da4d6b4b1d68c9556f37
roei910 <74923231+roei910@users.noreply.github.com>
2024-03-04 18:15:13 +0200

    adding logic to player service using the algorithm.

--- TakiApp/Services/Players/PlayerService.cs
+++ TakiApp/Services/Players/PlayerService.cs
@@ -1,28 +1,52 @@
-﻿using TakiApp.Interfaces;
+﻿using System.Drawing;
+using TakiApp.Interfaces;
 using TakiApp.Models;
 
 namespace TakiApp.Services.Players
 {
     public class PlayerService : IPlayerService
     {
-        public Card PickCard(Player currentPlayer, Card topDiscard)
+        private readonly List<IPlayerAlgorithm> _playerAlgorithms;
+
+        public PlayerService(List<IPlayerAlgorithm> playerAlgorithms)
         {
+            _playerAlgorithms = playerAlgorithms;
+        }
+
+        public Card? PickCard(Player currentPlayer, Func<Card, bool> canStackOnTopDiscard)
+        {
+            var algorithm = MatchAlgorithm(currentPlayer);
+
+            var chosenCard = algorithm.ChooseCard(canStackOnTopDiscard, currentPlayer.Cards);
+
+            return chosenCard;
+        }
+
+        public void AddCard(Player currentPlayer, Card card)
+        {
+
             throw new NotImplementedException();
         }
 
-        public void ChooseCard()
+        public Player PickOtherPlayer(Player currentPlayer, List<Player> players)
         {
+            var algorithm = MatchAlgorithm(currentPlayer);
 
-        }
-
-        public void AddCard(Card card)
-        {
             throw new NotImplementedException();
         }
 
-        public Player PickOtherPlayer(List<Player> players)
+        public Color ChooseColor(Player player)
         {
-            throw new NotImplementedException();
+            var algo = MatchAlgorithm(player);
+
+            return algo.ChooseColor(player.Cards);
+        }
+
+        private IPlayerAlgorithm MatchAlgorithm(Player player)
+        {
+            var found = _playerAlgorithms.Where(algo => algo.ToString() == player.PlayerAlgorithm).FirstOrDefault();
+
+            return found ?? throw new Exception("Couldnt find algorithm in the list");
         }
     }
 }
