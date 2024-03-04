39ba0573c1a975c2edd773e1d549b2e2325f5cef
roei910 <74923231+roei910@users.noreply.github.com>
2024-03-04 10:08:28 +0200

    adding functionality to game initializer

--- TakiApp/Services/GameLogic/GameInitializer.cs
+++ TakiApp/Services/GameLogic/GameInitializer.cs
@@ -1,19 +1,32 @@
-﻿using TakiApp.Interfaces;
+﻿using MongoDB.Bson;
+using Taki.Models.Algorithm;
+using TakiApp.Dal;
+using TakiApp.Factories;
+using TakiApp.Interfaces;
 using TakiApp.Models;
 
 namespace TakiApp.Services.GameLogic
 {
     public class GameInitializer : IGameInitializer
     {
-        private readonly IDal<GameSettings> _gameSettingsDal;
         private readonly IUserCommunicator _userCommunicator;
+        private readonly IGameSettingsRepository _gameSettingsRepository;
+        private readonly IPlayersRepository _playersRepository;
+        private readonly CardsFactory _cardsFactory;
+        private readonly IDrawPileRepository _drawPileRepository;
+        private readonly IDiscardPileRepository _discardPileRepository;
         private GameSettings? _gameSettings;
 
-        public GameInitializer(IDal<GameSettings> gameSettingsDal,
-            IUserCommunicator userCommunicator)
+        public GameInitializer(IUserCommunicator userCommunicator, IGameSettingsRepository gameSettingsRepository,
+            IPlayersRepository playersRepository, CardsFactory cardsFactory, IDrawPileRepository drawPileRepository, 
+            IDiscardPileRepository discardPileRepository)
         {
-            _gameSettingsDal = gameSettingsDal;
             _userCommunicator = userCommunicator;
+            _gameSettingsRepository = gameSettingsRepository;
+            _playersRepository = playersRepository;
+            _cardsFactory = cardsFactory;
+            _drawPileRepository = drawPileRepository;
+            _discardPileRepository = discardPileRepository;
         }
 
         public GameSettings GetGameSettings()
@@ -21,31 +34,83 @@ namespace TakiApp.Services.GameLogic
             return _gameSettings!;
         }
 
-        public void InitializeGame()
+        public async Task InitializeGame()
         {
-            var gameSettings = Task.Run(async () => await _gameSettingsDal.FindAsync()).Result;
+            _gameSettings = _gameSettingsRepository.GetGameSettingsAsync().Result;
 
-            if(gameSettings.Count == 0)
+            if(_gameSettings == null)
             {
                 _userCommunicator.SendMessageToUser("Building a new Taki Game!");
                 var isOnline = _userCommunicator.GetMessageFromUser("Please enter type of game: online or normal");
                 var typeOfGame = _userCommunicator.GetMessageFromUser("Please enter pyramid or normal");
                 var numberOfPlayerCards = _userCommunicator.GetNumberFromUser("Please enter number of player cards");
+                var numberOfPlayers = _userCommunicator.GetNumberFromUser("Please enter number of players");
 
                 _gameSettings = new GameSettings()
                 {
+                    Id = ObjectId.GenerateNewId(),
                     HasGameStarted = false,
                     IsOnline = isOnline == "online",
                     NumberOfPlayerCards = numberOfPlayerCards,
-                    TypeOfGame = typeOfGame
+                    TypeOfGame = typeOfGame,
+                    NumberOfPlayers = numberOfPlayers,
                 };
 
-                _gameSettingsDal.CreateOneAsync(_gameSettings);
-                return;
+                await _gameSettingsRepository.CreateGameSettings(_gameSettings);
+
+                var cards = _cardsFactory.GenerateDeck();
+
+                await _drawPileRepository.AddManyRandomAsync(cards);
+                await InitializeCards();
             }
 
             //TODO: game already setting up in the background we need to check if we can enter or not.
-            _gameSettings = gameSettings.First();
+            if (!_gameSettings.IsOnline)
+            {
+                await InitializeNormal();
+
+                return;
+            }
+
+        }
+
+        private async Task InitializeNormal()
+        {
+            var players =  Enumerable.Range(0, _gameSettings!.NumberOfPlayers)
+                .Select(x =>
+                {
+                    Player player = new Player()
+                    {
+                        Id = ObjectId.GenerateNewId(),
+                        Name = _userCommunicator.GetMessageFromUser("Please enter a name for your player"),
+                        LastCheckIn = DateTime.UtcNow,
+                        PlayerAlgorithm = typeof(ManualPlayerAlgorithm).ToString(),
+                        Cards = [],
+                        IsPlaying = false
+                    };
+
+                    return player;
+                }).ToList();
+
+            await _playersRepository.CreateManyAsync(players);
+            await InitializeCards();
+
+            players.ForEach(async p =>
+            {
+                while (p.Cards.Count < _gameSettings.NumberOfPlayerCards)
+                    await _playersRepository.PlayerDrawCardAsync(p);
+            });
+        }
+
+        private async Task InitializeCards()
+        {
+            await _drawPileRepository.DeleteAllAsync();
+            await _discardPileRepository.DeleteAllAsync();
+
+            await _drawPileRepository.AddManyRandomAsync(_cardsFactory.GenerateDeck());
+            var drawCard = await _drawPileRepository.DrawCardAsync();
+
+            await _discardPileRepository.AddCard(drawCard);
         }
     }
 }
