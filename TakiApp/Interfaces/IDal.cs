5259522c8a98303aac1dd233ce84b6c871d6e66d
roei910 <74923231+roei910@users.noreply.github.com>
2024-03-05 15:24:36 +0200

    update to dal

--- TakiApp/Interfaces/IDal.cs
+++ TakiApp/Interfaces/IDal.cs
@@ -7,6 +7,7 @@ namespace TakiApp.Interfaces
         Task CreateOneAsync(T value);
         Task CreateManyAsync(List<T> values);
         Task UpdateOneAsync(T valueToUpdate);
+        Task UpdateManyAsync(List<T> valuesToUpdate);
         Task<List<T>> FindAsync();
         Task<T> FindOneAsync(ObjectId objectId);
         Task DeleteAsync(T value);
