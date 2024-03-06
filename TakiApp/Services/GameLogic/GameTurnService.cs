f7435fae69a3928eff288b1b36833fcceaa9ff19
roei910 <74923231+roei910@users.noreply.github.com>
2024-03-06 16:52:10 +0200

    adding finish no play, fixing draw cards

--- TakiApp/Services/GameLogic/GameTurnService.cs
+++ TakiApp/Services/GameLogic/GameTurnService.cs
@@ -1,5 +1,4 @@
 ﻿using MongoDB.Bson;
-using System.Drawing;
 using TakiApp.Interfaces;
 using TakiApp.Models;
 
@@ -10,45 +9,54 @@ namespace TakiApp.Services.GameLogic
         private readonly IPlayersRepository _playerRepository;
         private readonly IAlgorithmService _algorithmService;
         private readonly IDiscardPileRepository _discardPileRepository;
-        //TODO: not used check if needed
-        private readonly IDrawPileRepository _drawPileRepository;
         private readonly IUserCommunicator _userCommunicator;
         private readonly ICardPlayService _cardPlayService;
 
         public GameTurnService(IPlayersRepository playerRepository, IAlgorithmService algorithmService,
-            IDiscardPileRepository discardPileRepository, IDrawPileRepository drawPileRepository,
-            IUserCommunicator userCommunicator, ICardPlayService cardPlayService)
+            IDiscardPileRepository discardPileRepository, IUserCommunicator userCommunicator, 
+            ICardPlayService cardPlayService)
         {
             _playerRepository = playerRepository;
             _algorithmService = algorithmService;
             _discardPileRepository = discardPileRepository;
-            _drawPileRepository = drawPileRepository;
             _userCommunicator = userCommunicator;
             _cardPlayService = cardPlayService;
         }
 
         public async Task PlayTurnByIdAsync(ObjectId playerId)
         {
+            //TODO: need to add the reset card
             var topDiscard = await _discardPileRepository.GetTopDiscardAsync();
             var currentPlayer = await _playerRepository.GetCurrentPlayerAsync();
 
-            _userCommunicator.SendAlertMessage($"Top discard: {topDiscard.Type}, {Color.FromName(topDiscard.CardColor)}");
+            _userCommunicator.SendAlertMessage($"Top discard: {topDiscard}");
             _userCommunicator.SendAlertMessage($"Current player: {currentPlayer.Name}");
 
             var canStack = _cardPlayService.CanStack(topDiscard);
-            Card? card = _algorithmService.PickCard(currentPlayer, canStack);
+            var cardsToDraw = _cardPlayService.CardsToDraw(topDiscard);
+
+            Card? card = _algorithmService.PickCard(currentPlayer, canStack, $"or draw {cardsToDraw} card(s)");
             
             if (card is null)
             {
-                await _playerRepository.DrawCardsAsync(currentPlayer, _cardPlayService.CardsToDraw(topDiscard));
+                await _cardPlayService.FinishNoPlay(topDiscard);
+
+                var cardsDrew = await _playerRepository.DrawCardsAsync(currentPlayer, cardsToDraw);
+                if(cardsDrew.Count == 0)
+                {
+                    _userCommunicator.SendErrorMessage("Couldnt draw cards from deck");
+                    await _playerRepository.NextPlayerAsync();
+
+                    return;
+                }
+                await _playerRepository.SendMessagesFromPlayerAsync(currentPlayer, $"{currentPlayer.Name} drew {cardsDrew.Count} card(s)\n");
                 await _playerRepository.NextPlayerAsync();
-                await _playerRepository.AddMessagesToPlayersAsync(currentPlayer, $"{currentPlayer.Name} drew a card\n");
 
                 return;
             }
 
             currentPlayer.Cards.Remove(card);
-            await _playerRepository.UpdatePlayer(currentPlayer);
+            await _playerRepository.UpdatePlayerAsync(currentPlayer);
             await _discardPileRepository.AddCardAsync(card);
 
             await _cardPlayService.PlayCardAsync(currentPlayer, card);
@@ -63,7 +71,7 @@ namespace TakiApp.Services.GameLogic
 
             player.Messages.Clear();
 
-            await _playerRepository.UpdatePlayer(player);
+            await _playerRepository.UpdatePlayerAsync(player);
 
             if (player != null && player.IsPlaying)
                 return;
