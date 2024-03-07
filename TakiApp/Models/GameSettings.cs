5e3d065bc755a9b4ba278b736831a7c4875e66bf
roei910 <74923231+roei910@users.noreply.github.com>
2024-03-07 15:11:37 +0200

    adding fields to game settings

--- TakiApp/Models/GameSettings.cs
+++ TakiApp/Models/GameSettings.cs
@@ -9,6 +9,9 @@ namespace TakiApp.Models
         public int NumberOfPlayers { get; set; }
         public string? TypeOfGame { get; set; }
         public bool IsOnline { get; set; }
-        public bool HasGameStarted { get; set; }
+        public int NumberOfWinners { get; set; }
+        public bool HasGameStarted { get; set; } = false;
+        public bool HasGameEnded { get; set; } = false;
+        public List<string> winners { get; set; } = [];
     }
 }
